a59c1b8f61fa266355531a2f021ef559a98d7c52
Mefistofel <Mfstfl@gmail.com>
2019-08-11 16:42:20 +0300

    Initial commit - homebrew physics scene

---
+++ Assets/Sources/HomebrewPhysics/Ball.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public sealed class Ball : MonoBehaviour {
+
+    public float Radius = 1f;
+    public Vector3 Velocity = Vector3.zero;
+
+    public Vector3 CollisionVelocity = Vector3.zero;
+    public Vector3 Correction = Vector3.zero;
+
+    public void Move(float deltaTime) {
+        transform.position = transform.position + Velocity * deltaTime;
+    }
+
+    public void CheckCollision(Ball other) {
+        if (IsCollided(other)) {
+            var delta = transform.position - other.transform.position;
+            var normal = delta.normalized;
+            Correction = (delta - normal * (Radius + other.Radius)) * -0.5f;
+            var projection = Vector3.Dot(Velocity, normal);
+            var impulseVelocity = -normal * projection;
+            CollisionVelocity += impulseVelocity;
+            other.CollisionVelocity -= impulseVelocity;
+        }
+    }
+
+    public void ApplyCollision() {
+        Velocity += CollisionVelocity;
+        CollisionVelocity = Vector3.zero;
+
+        transform.position += Correction;
+        Correction = Vector3.zero;
+    }
+
+    public bool IsCollided(Ball other) {
+        var actualDistance = Vector3.Distance(other.transform.position, transform.position);
+        var minimalDistance = Radius + other.Radius;
+        return (actualDistance < minimalDistance);
+    }
+
+    public void CheckBordersCollision(Rect borders) {
+        var position = transform.position;
+        if (position.x < borders.xMin + Radius) { // right
+            position.x = borders.xMin + Radius;
+            Velocity.x = -Velocity.x;
+        }
+        if (position.x > borders.xMax - Radius) { // left
+            position.x = borders.xMax - Radius;
+            Velocity.x = -Velocity.x;
+        }
+        if (position.y < borders.yMin + Radius) { // bottom
+            position.y = borders.yMin + Radius;
+            Velocity.y = -Velocity.y;
+        }
+        if (position.y > borders.yMax - Radius) { // top
+            position.y = borders.yMax - Radius;
+            Velocity.y = -Velocity.y;
+        }
+        transform.position = position;
+    }
+}
---
+++ Assets/Sources/HomebrewPhysics/PhysicalController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public sealed class PhysicalController : MonoBehaviour {
+    [Header("Generation params")]
+    public Ball ballPrefab;
+    public float MinRadius = 1f;
+    public float MaxRadius = 1f;
+    public int Count = 20;
+
+    [Header("Physical props")]
+    public Rect borders = new Rect(-10, -10, 20, 20);
+    public Vector3 gravity = Vector3.zero;
+
+    public List<Ball> balls = new List<Ball>();
+
+    private void Start() {
+        for (int i = 0; i < Count; i++) {
+            var position = new Vector2(Random.Range(borders.xMin, borders.xMax), Random.Range(borders.yMin, borders.yMax));
+            var velocity = Random.insideUnitSphere * 10;
+            var radius = Random.Range(MinRadius, MaxRadius);
+            balls.Add(CreateBall(ballPrefab, position, velocity, radius));
+        }
+    }
+
+    private void Update() {
+        foreach (var ball in balls)
+            ball.Velocity += gravity * Time.deltaTime;
+
+        foreach (var ball in balls)
+            ball.Move(Time.deltaTime);
+
+
+        foreach (var ballA in balls) {
+            foreach (var ballB in balls) {
+                if (ballA != ballB) {
+                    ballA.CheckCollision(ballB);
+                }
+            }
+        }
+
+        foreach (var ball in balls)
+            ball.ApplyCollision();
+
+        foreach (var ball in balls)
+            ball.CheckBordersCollision(borders);
+
+    }
+
+
+    private Ball CreateBall(Ball prefab, Vector2 position, Vector2 velocity, float radius = 1f) {
+        var ball = Instantiate(prefab, transform);
+        ball.Velocity = velocity;
+        ball.Radius = radius;
+        ball.transform.position = position;
+        ball.transform.localScale = Vector3.one * radius;
+        return ball;
+    }
+
+
+    private void OnDrawGizmos() {
+        // Draw borders
+        var topLeft  = new Vector3(borders.xMin, borders.yMax);
+        var topRight = new Vector3(borders.xMax, borders.yMax);
+        var bottomLeft = new Vector3(borders.xMin, borders.yMin);
+        var bottomRight = new Vector3(borders.xMax, borders.yMin);
+
+        Gizmos.DrawLine(topLeft, topRight);
+        Gizmos.DrawLine(topRight, bottomRight);
+        Gizmos.DrawLine(bottomRight, bottomLeft);
+        Gizmos.DrawLine(bottomLeft, topLeft);
+    }
+}
