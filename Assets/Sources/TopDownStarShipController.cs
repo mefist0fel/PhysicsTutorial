d7d19456f680eb55bbffdef737d35f23b0870844
Mefistofel <Mfstfl@gmail.com>
2019-08-11 20:01:41 +0300

    Added starship physics

---
+++ Assets/Sources/TopDownStarShipController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public sealed class TopDownStarShipController : MonoBehaviour {
+    public float moveForce = 10f;
+    public float rotationForce = 50f;
+    public Rigidbody2D rigidbody2d;
+
+
+    void Start() {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        ControlMove();
+    }
+
+    private void ControlMove() {
+        if (Input.GetKey(KeyCode.W))
+            rigidbody2d.AddForce(transform.rotation * Vector3.up * moveForce);
+        if (Input.GetKey(KeyCode.S))
+            rigidbody2d.AddForce(transform.rotation * Vector3.down * moveForce);
+
+        if (Input.GetKey(KeyCode.D))
+            rigidbody2d.AddTorque(-rotationForce * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            rigidbody2d.AddTorque( rotationForce * Time.deltaTime);
+
+    }
+}
---
+++ Assets/Sources/TopDownVehicleController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public sealed class TopDownVehicleController : MonoBehaviour {
+    public float speed = 10f;
+    public float rotationSpeed = 10f;
+    public Rigidbody2D rigidbody2d;
+
+
+    void Start() {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        ControlMove();
+    }
+
+    private void ControlMove() {
+        Vector3 moveDirection = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+            moveDirection += Vector3.up;
+        if (Input.GetKey(KeyCode.S))
+            moveDirection += Vector3.down;
+
+        rigidbody2d.MovePosition(transform.position + transform.rotation * moveDirection * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            rigidbody2d.MoveRotation(rigidbody2d.rotation - rotationSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            rigidbody2d.MoveRotation(rigidbody2d.rotation + rotationSpeed * Time.deltaTime);
+
+    }
+}
