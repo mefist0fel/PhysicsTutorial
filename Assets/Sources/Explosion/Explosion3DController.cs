cd3b29259ae9e5dd4f14db721bcf733f735b22d0
Mefistofel <Mfstfl@gmail.com>
2019-08-17 19:06:03 +0300

    Added explosion 3d demo

---
+++ Assets/Sources/Explosion/Explosion3DController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion3DController : MonoBehaviour {
+    public Camera controlCamera;
+    public float explosionRadius = 10f;
+    public float explosionForce = 10f;
+    public float upwardsModifier = 3f;
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            var ray = controlCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                ExplodeInPoint(hit.point);
+            }
+        }
+    }
+
+    private void ExplodeInPoint(Vector3 position) {
+        Collider[] colliders = Physics.OverlapSphere(position, explosionRadius);
+        foreach (Collider hit in colliders) {
+            Rigidbody rigidbody = hit.GetComponent<Rigidbody>();
+
+            if (rigidbody != null)
+                rigidbody.AddExplosionForce(explosionForce, position, explosionRadius, upwardsModifier);
+        }
+    }
+}
--- Assets/Sources/Sandbox/ObjectGenerator.cs
+++ Assets/Sources/Sandbox/ObjectGenerator.cs
@@ -2,11 +2,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ObjectGenerator : MonoBehaviour {
     public GameObject prefab;
     public int Count = 10;
     public float Delay = 0.2f;
+    public float Offset = 1f;
 
     float timer = 0;
     int spawnedCount = 0;
@@ -22,7 +24,7 @@ public class ObjectGenerator : MonoBehaviour {
 
     private void SpawnObject(GameObject prefab) {
         var obj = Instantiate(prefab);
-        obj.transform.position = transform.position;
+        obj.transform.position = transform.position + Random.insideUnitSphere * Offset;
         spawnedCount += 1;
     }
 }
